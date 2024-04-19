2f607623c4f1f6b70ff20c8fc0a7f648ac009452
tomibrg <162723299+tomibrg@users.noreply.github.com>
2024-04-19 12:04:20 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,12 +1,13 @@
 def leap_year():
     year = int(input('Ingrese un año: '))
-    is_leap_year_msg = ''
-    
-    if (year % 100 == 0 and year % 400 == 0) or (year % 4 == 0 and not (year % 100 == 0)):
-         is_leap_year_msg = f'El año {year} es bisiesto'
-     else:
-         is_leap_year_msg = f'El año {year} no es bisiesto'
-     print(is_leap_year_msg)
+    is_leap_year_msg = 'hello'
+    if (year % 100 == 0) and (year % 400 == 0):
+        is_leap_year_msg = f'El año {year} es bisiesto'
+    elif year % 4 == 0 and not (year % 100 == 0):
+        is_leap_year_msg = f'El año {year} es bisiesto'
+    else:
+        is_leap_year_msg = f'El año {year} no es bisiesto'
+    print(is_leap_year_msg)
 
 
 
